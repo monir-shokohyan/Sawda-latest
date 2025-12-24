c400ec558648d8ffb1240757dab06a5a792cdfe1
m monir shokohyan <monir.shekoyans1@gmail.com>
2025-12-24 15:36:11 +0300

    fix: fix the issue of havign deferrent color in product details page

--- src/features/product-details/ui/ui.tsx
+++ src/features/product-details/ui/ui.tsx
@@ -1,9 +1,9 @@
-import { Anchor, Breadcrumbs, Stack, Text } from '@mantine/core'
+import { Button, Flex, Group, Stack, Text } from '@mantine/core'
 import { useMediaQuery } from '@mantine/hooks'
 import BreadcrumbsNav from '@shared/bread-crumb/breadcrumb'
 import { GeneralPadding } from '@shared/constants'
-import { FaHome } from 'react-icons/fa'
-import { useSearchParams, Link } from 'react-router-dom'
+import { MdEditDocument, MdOutlineFavorite } from 'react-icons/md'
+import { useSearchParams } from 'react-router-dom'
 
 function Ui() {
   const isMobile = useMediaQuery('(max-width: 768px)')
@@ -17,6 +17,22 @@ function Ui() {
       p={isMobile ? 'md' : GeneralPadding}
     >
       <BreadcrumbsNav items={[{ title: 'Home', href: '/' }, { title: name }]} />
+      <Flex gap={10} p={GeneralPadding}>
+        <Stack  w="65%" >
+            <Text>2019 Toyota Land Cruiser Prado 2.8 AT 112,000 km</Text>
+            <Flex gap={10}>
+              <Button  color="background" c="textPrimary" leftSection={<MdOutlineFavorite />}><Text size="sm">
+                Add to favorites
+                </Text></Button>
+              <Button  color="background" c="textPrimary" leftSection={<MdEditDocument />}><Text size="sm">
+                Add a note
+                </Text></Button>
+            </Flex>
+        </Stack>
+        <Group  w="35%" >
+
+        </Group>
+      </Flex>
     </Stack>
   )
 }
