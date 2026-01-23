400b8bd0d452dab66a0fe8c7f6a55067ac91dc75
m monir shokohyan <monir.shekoyans1@gmail.com>
2026-01-23 17:54:00 +0300

    fix: fix the lazy image option in website

--- src/shared/ui/image/LazyImage.tsx
+++ src/shared/ui/image/LazyImage.tsx
@@ -6,31 +6,22 @@ import {
 interface Props extends LazyLoadImageProps {
   src: string
   alt: string
-  className?: string
-  style?: React.CSSProperties
 }
 
-const LazyImage = ({ src, alt, className = '', style, ...props }: Props) => {
-  return (
-  
-      <LazyLoadImage
-        alt={alt}
-        effect="blur"
-        src={src}
-        placeholderSrc="/cover.png"
-        style={{
-          objectFit: 'cover',
-          width: '100%',
-          height: '100%',
-          display: 'block',
-          overflow: 'hidden',
-        }}
-        wrapperProps={{
-          style: { width: '100%', height: '100%', overflow: 'hidden' },
-        }}
-        {...props}
-      />
-  )
-}
+const LazyImage = ({ src, alt, ...props }: Props) => (
+  <LazyLoadImage
+    alt={alt}
+    effect="blur"
+    style={{ objectFit: 'cover', overflow: 'hidden'}}
+    src={src}
+    placeholderSrc="/cover.png"
+    wrapperProps={{
+        style: {
+            overflow: 'hidden'
+        }
+    }}
+    {...props}
+  />
+)
 
 export { LazyImage }
