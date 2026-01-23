07f3f5cc7f3dc3a54e2613fdfcde1dd31c48364d
m monir shokohyan <monir.shekoyans1@gmail.com>
2026-01-23 17:45:45 +0300

    fix: fix the lazy image option in website

--- src/shared/ui/image/LazyImage.tsx
+++ src/shared/ui/image/LazyImage.tsx
@@ -1,22 +1,44 @@
-import {
-  LazyLoadImage,
-  LazyLoadImageProps,
-} from 'react-lazy-load-image-component'
+import { LazyLoadImage, LazyLoadImageProps } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 interface Props extends LazyLoadImageProps {
-  src: string
-  alt: string
+  src: string;
+  alt: string;
+  className?: string;
+  style?: React.CSSProperties;
 }
 
-const LazyImage = ({ src, alt, ...props }: Props) => (
-  <LazyLoadImage
-    alt={alt}
-    effect="blur"
-    style={{ objectFit: 'cover', zIndex: -10 }}
-    src={src}
-    placeholderSrc="/cover.png"
-    {...props}
-  />
-)
+const LazyImage = ({ src, alt, className = '', style, ...props }: Props) => {
+  return (
+    <div
+      style={{
+        position: 'relative',
+        overflow: 'hidden',
+        borderRadius: 'inherit',
+        width: '100%',
+        height: '100%',
+        ...style,
+      }}
+      className={className}
+    >
+      <LazyLoadImage
+        alt={alt}
+        effect="blur"
+        src={src}
+        placeholderSrc="/cover.png"
+        style={{
+          objectFit: 'cover',
+          width: '100%',
+          height: '100%',
+          display: 'block',
+        }}
+        wrapperProps={{
+          style: { width: '100%', height: '100%' },
+        }}
+        {...props}
+      />
+    </div>
+  );
+};
 
-export { LazyImage }
+export { LazyImage };
