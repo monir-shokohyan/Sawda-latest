cee5579f24e2ae604b49d47e977b762228cbc99c
m monir shokohyan <monir.shekoyans1@gmail.com>
2026-01-24 08:59:32 +0300

    fix: fix the issue of the code

--- src/shared/ui/image/LazyImage.tsx
+++ src/shared/ui/image/LazyImage.tsx
@@ -1,55 +1,54 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import {
   LazyLoadImage,
   LazyLoadImageProps,
-} from 'react-lazy-load-image-component';
+} from 'react-lazy-load-image-component'
 
 interface Props extends LazyLoadImageProps {
-  src: string;
-  alt: string;
-  fallbackSrc?: string;
+  src: string
+  alt: string
+  fallbackSrc?: string
 }
 
 const LazyImage = ({
   src,
   alt,
-  fallbackSrc = '/cover.png',
+  fallbackSrc = '/logo.png',
   ...props
 }: Props) => {
-  const [imgSrc, setImgSrc] = useState(src);
-  const [hasError, setHasError] = useState(false);
+  const [imgSrc, setImgSrc] = useState(src)
 
   const handleError = () => {
-    if (!hasError) {
-      setImgSrc(fallbackSrc);
-      setHasError(true);
+    if (imgSrc !== fallbackSrc) {
+      setImgSrc(fallbackSrc)
     }
-  };
+  }
 
   return (
-    <div style={{width: '100%', height: '100%', overflow: 'hidden'}}>
-        <LazyLoadImage
-          alt={alt}
-          effect="blur"
-          src={imgSrc}
-          placeholderSrc={fallbackSrc}
-          onError={handleError}
-          style={{
-            objectFit: 'cover',
-          }}
-          wrapperProps={{
-            style: {
-              width: '100%',
-              height: '100%',
-              overflow: 'hidden',
-              borderRadius: 'inherit',
-              transitionDelay: '1s',
-            },
-          }}
-          {...props}
-        />
+    <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
+      <LazyLoadImage
+        key={imgSrc}
+        alt={alt}
+        effect="blur"
+        src={imgSrc}
+        placeholderSrc={fallbackSrc}
+        onError={handleError}
+        style={{
+          objectFit: 'cover',
+        }}
+        wrapperProps={{
+          style: {
+            width: '100%',
+            height: '100%',
+            overflow: 'hidden',
+            borderRadius: 'inherit',
+            transitionDelay: '1s',
+          },
+        }}
+        {...props}
+      />
     </div>
-  );
-};
+  )
+}
 
-export { LazyImage };
+export { LazyImage }
